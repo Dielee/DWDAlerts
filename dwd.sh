d4d98775738ecdab0292d64dae0086b1af534f64
Dielee <45101649+Dielee@users.noreply.github.com>
2019-07-15 14:20:16 +0200

    Changed date format to dots

--- dwd.sh
+++ dwd.sh
@@ -10,8 +10,8 @@ data=$(curl -s $url | cut -d '(' -f 2- | rev | cut -c 3- | rev | \
 jq --arg b "$bundesland" --arg k "$kreis" '.warnings[] | .[] | select(.state == $b) | select(.regionName == $k) | {start,"end",type,level,event,state,regionName,headline,description}')
 
 # Calc starttime and endtime from warnings (MESZ)
-start=$(echo $data | jq --argjson f "$fixtime" '.start/1000 + $f | strftime("%d-%m-%Y %H:%M")' | tr -d '"' )
-end=$(echo $data | jq --argjson f "$fixtime" '.end/1000 + $f | strftime("%d-%m-%Y %H:%M")' | tr -d '"' )
+start=$(echo $data | jq --argjson f "$fixtime" '.start/1000 + $f | strftime("%d.%m.%Y %H:%M")' | tr -d '"' )
+end=$(echo $data | jq --argjson f "$fixtime" '.end/1000 + $f | strftime("%d.%m.%Y %H:%M")' | tr -d '"' )
 
 # Insert start and end to data
 data=$(echo $data | jq --arg s "$start" --arg e "$end" '{type,level,event,state,regionName,headline,description} | . |= . + {"start": $s, "end": $e}')
